8cee55856b3b7a641375e871f65494b5eb696bd4
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-03 18:05:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(words)
-  n = 0
-  while n<words.size
-       words[n]
-       n+=1# code here
+  counter = 0
+  while counter<words.size
+       words[counter]
+       counter+=1# code here
 end
 words
 end
