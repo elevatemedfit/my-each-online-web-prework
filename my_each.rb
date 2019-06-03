2cfff5b06ac73e7d6e5ac2ea040ac4c22fdadd46
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-03 18:18:42 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  n = 0
+  while n<words.size
+    yield(words.size)
+        words[n]
+       n+=1# code here
+end
+words
 end
